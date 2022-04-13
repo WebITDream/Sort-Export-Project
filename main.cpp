707d3c44cee5436b3c6b830cc8a274a20c1c779b
WebIT <51296252+WebITDream@users.noreply.github.com>
2022-04-13 19:08:25 +0300

    Version 0.2 Export/Clean
    Version 0.2 Export/Clean

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <sstream>
 #include <fstream>
+#include <ctime> 
 
 using namespace std;
 
@@ -12,32 +13,42 @@ int main(){
     ifstream file;
     openFile(file, "text.txt");
 
+
     // procesare fisier;
     processFile(file);
+
+
 }
 
 void processFile(ifstream& file){
     string line, word;
     istringstream iss;
 
-    // string keyword[22] = {"anicacjtm", "bukr", "cityhome", "IC", "insula", "interviu", "msb", "napoleon", "radu", "rveOR", "rve-or",
-    //  "rve-beta", "rvesb", "rveTM", "rve-tm", "omma", "pcma", "pima", "vnma"};
+    string keyword[24] = {"anicacjtm", "/bukr", "/napoleon", "bukr", "cityhome", "IC", "insula", "interviu", "msb", "napoleon", "radu", "rveOR", "rve-or", "rve-beta", "rvesb", "rveTM", "rve-tm", "omma", "pcma", "pima", "vnma", "/rveOR", "/rve-or", "/rve-tm"};
 
+    ofstream exportFile;
+    exportFile.open("export.txt");
 
     while(!file.eof()){
         getline(file, line);
 // Ia fiecare linie si imparte in cuvinte
+
         iss.clear();
         iss.str(line);
             while(iss.good()){
                 iss >> word;
                 // verificarea daca este X cuvant, daca este se afiseaza
-                if(word == "napoleon" || word == "rve-tm" || word == "rveOR" || word == "bukr" || word == "rveor"){
-                    cout << word << endl;
+                for(int i=0;i<=22;i++)
+                    if(word == keyword[i]){
+                        exportFile << word << endl;
+                    }
                 }
             }
-
-    }
+            time_t tmNow = time(0);
+            char *dt = ctime(&tmNow);
+            exportFile << "Data si timpul cand a fost efectuat raportul: " << dt << endl;
+            cout << "Apasa orice tasta pentru a parasii executabilulul, rezultatul il poti gasi in fisierul: export.txt !";
+            cin.get();
 }
 
 
